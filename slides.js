27b7ac8649323b28b06b30e3302d64d5ff2188bd
Monica Benitez <mony.23@hotmail.com>
2014-05-29 00:58:03 -0500

    Modificaciones en la pagina

--- slides.js
+++ slides.js
@@ -5,25 +5,6 @@ $(".a").hover(function() {
   }, function() {
     $(this).removeClass("hover");
     $(this).siblings().removeClass("unhover");
-  }
-);
+  });
 
-$(".a").click(function(){
-  $(this).toggleClass("click");
-  $(this).siblings().toggleClass("unclick");
-  $(this).children("p").fadeToggle(150);
-}).children().click(function(e) {
-  return false;
-});
-
-setTimeout(function(){
-  $('.a').not('.a:nth-child(3)').addClass('unclick');
-  $('.a:nth-child(3)').addClass('click');
-  $('.a:nth-child(3)').children("p").fadeToggle(150);
-},700);
-setTimeout(function(){
-  $('.a').not('.a:nth-child(3)').removeClass('unclick');
-  $('.a:nth-child(3)').removeClass('click');
-  $('.a:nth-child(3)').children("p").fadeToggle(150);
-},1700);
 });
