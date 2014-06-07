92875cde903ffba0bb6eff9bf864e8a3b53ad953
Miguel Angel Gordian <os.aioria@gmail.com>
2014-06-07 18:27:58 -0500

    Ejemplo de template para sitio

---
+++ template.php
@@ -0,0 +1,11 @@
+<?php
+include 'header.html';
+#foreach ([1,2,3,4,5,6] as $n) {
+#  echo "<h1>Numero {$n}</h1><br>\n";
+#}
+include 'Slides.html';
+?>
+<h1>KMG SYSTEM</h1>
+<?php
+include 'footer.html';
+?>
